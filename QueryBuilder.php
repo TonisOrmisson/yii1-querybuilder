ce50fb8ac003903ba4bb69400f987a5cc9711a4f
Tõnis Ormisson <tonis@andmemasin.eu>
2017-07-18 09:49:02 +0300

    Add readme

--- QueryBuilder.php
+++ QueryBuilder.php
@@ -1,12 +1,52 @@
 <?php
 
 /**
- * Created by PhpStorm.
- * User: tonis_o
- * Date: 18.07.17
- * Time: 0:13
+ * @author Tõnis Ormisson <tonis@andmemasin.eu>
  */
-class QueryBuilder
+class QueryBuilder extends CWidget
 {
+    /** @var  string QueryBuilder container ID */
+    public $id;
 
+    /** @var array $filters */
+    public $filters;
+
+
+    public $cssFile;
+    private $jsFile;
+
+    /** @inheritdoc */
+    public function init()
+    {
+        $cssFileName=dirname(__FILE__).DIRECTORY_SEPARATOR.'query-builder'.DIRECTORY_SEPARATOR.'css'.DIRECTORY_SEPARATOR.'query-builder.default.css';
+        $jsFileName=dirname(__FILE__).DIRECTORY_SEPARATOR.'query-builder'.DIRECTORY_SEPARATOR.'js'.DIRECTORY_SEPARATOR.'query-builder.standalone.js';
+        $this->cssFile=Yii::app()->getAssetManager()->publish($cssFileName);
+        $this->jsFile = Yii::app()->getAssetManager()->publish($jsFileName);
+        $this->registerQueryBuilderAssets();
+
+        if(!$this->id){
+            $this->id = "query-builder";
+        }
+        $this->filters =array(array('id'=>1,'label'=>'test','type'=>'string'));
+
+        parent::init();
+    }
+
+    /** @inheritdoc */
+    public function run(){
+        $params = array(
+            'builderId' => $this->id,
+            'filters' => json_encode($this->filters),
+        );
+        $this->render('builder',$params);
+    }
+
+
+    protected function registerQueryBuilderAssets()
+    {
+        // register assets
+        $cs=\Yii::app()->clientScript;
+        $cs->registerCssFile($this->cssFile);
+        $cs->registerScriptFile($this->jsFile, CClientScript::POS_HEAD);
+    }
 }
--- views/builder.php
+++ views/builder.php
@@ -1,2 +1,18 @@
 <?php
-echo 'skjdfdg';
+/** @var string $builderId */
+/** @var string $filters */
+?>
+<?php Yii::app()->clientScript->registerCoreScript('jquery');
+$builderId = "sadsdf";
+Yii::app()->clientScript->registerScript('build-query',
+<<<JS
+    $('#$builderId').queryBuilder({
+        plugins: ['bt-tooltip-errors'],
+        filters: $filters
+    });
+
+JS
+, CClientScript::POS_READY);
+
+?>
+<div id="<?=$builderId?>"></div>
