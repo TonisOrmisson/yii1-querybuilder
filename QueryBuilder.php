79f98de0f7f9fc811b439d1455bf6a4957f3b550
Tõnis Ormisson <tonis@andmemasin.eu>
2017-07-19 09:21:49 +0300

    fix Translator

--- QueryBuilder.php
+++ QueryBuilder.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /**
  * @author Tõnis Ormisson <tonis@andmemasin.eu>
  */
--- Translator.php
+++ Translator.php
@@ -2,8 +2,9 @@
 
 /**
  * Class Translator
- * @param array $params
- * @param array $where
+ * @see based on https://github.com/leandrogehlen/yii2-querybuilder/blob/master/Translator.php
+ * @property  array $params
+ * @property  array $where
  */
 class Translator extends CModel
 {
@@ -21,12 +22,11 @@ class Translator extends CModel
      */
     public function __construct($data, $config = [])
     {
+        // Yii1 does not init (its based on yii2 version)
+        $this->init();
         $this->_where = $this->buildWhere($data);
     }
 
-    /**
-     * @inheritdoc
-     */
     public function init()
     {
         $this->_operators = [
@@ -53,8 +53,7 @@ class Translator extends CModel
     }
 
     /**
-     * Returns the list of attribute names of the model.
-     * @return array list of attribute names.
+     * @inheritdoc
      */
     public function attributeNames()
     {
@@ -87,29 +86,28 @@ class Translator extends CModel
             }
             $pattern = $op;
         }
-
+        // Set params
         $this->_params = array_merge($this->_params, $params);
         return $field . " " . ($replacement ? str_replace("?", $replacement, $pattern) : $pattern);
     }
 
     /**
      * @param array $data rules configuration
-     * @return string the WHERE clause
+     * @return string the SQL WHERE statement
      */
     protected function buildWhere($data)
     {
         if (!isset($data['rules']) || !$data['rules']) {
-            return '';
+            return null;
         }
 
-        $this->criteria =  new CDbCriteria();
+        $criteria =  new CDbCriteria();
 
-        $where = [];
         $condition = " " . $data['condition'] . " ";
 
         foreach ($data['rules'] as $rule) {
             if (isset($rule['condition'])) {
-                $where[] = $this->buildWhere($rule);
+                $criteria->addCondition($this->buildWhere($rule),$condition);
             } else {
                 $params = [];
                 $operator = $rule['operator'];
@@ -128,10 +126,13 @@ class Translator extends CModel
                         $i++;
                     }
                 }
-                $where[] = $this->encodeRule($field, $operator, $params);
+                $mCondition = $this->encodeRule($field, $operator, $params);
+                $criteria->addCondition($mCondition,$condition);
             }
         }
-        return "(" . implode($condition, $where) . ")";
+
+        return $criteria->condition;
+
     }
 
     /**
@@ -154,42 +155,7 @@ class Translator extends CModel
 
 
     /**
-     * Retrieves the value of an array element or object property with the given key or property name.
-     * If the key does not exist in the array or object, the default value will be returned instead.
-     *
-     * The key may be specified in a dot format to retrieve the value of a sub-array or the property
-     * of an embedded object. In particular, if the key is `x.y.z`, then the returned value would
-     * be `$array['x']['y']['z']` or `$array->x->y->z` (if `$array` is an object). If `$array['x']`
-     * or `$array->x` is neither an array nor an object, the default value will be returned.
-     * Note that if the array already has an element `x.y.z`, then its value will be returned
-     * instead of going through the sub-arrays. So it is better to be done specifying an array of key names
-     * like `['x', 'y', 'z']`.
-     *
-     * Below are some usage examples,
-     *
-     * ```php
-     * // working with array
-     * $username = \yii\helpers\ArrayHelper::getValue($_POST, 'username');
-     * // working with object
-     * $username = \yii\helpers\ArrayHelper::getValue($user, 'username');
-     * // working with anonymous function
-     * $fullName = \yii\helpers\ArrayHelper::getValue($user, function ($user, $defaultValue) {
-     *     return $user->firstName . ' ' . $user->lastName;
-     * });
-     * // using dot format to retrieve the property of embedded object
-     * $street = \yii\helpers\ArrayHelper::getValue($users, 'address.street');
-     * // using an array of keys to retrieve the value
-     * $value = \yii\helpers\ArrayHelper::getValue($versions, ['1.0', 'date']);
-     * ```
-     *
-     * @param array|object $array array or object to extract value from
-     * @param string|\Closure|array $key key name of the array element, an array of keys or property name of the object,
-     * or an anonymous function returning the value. The anonymous function signature should be:
-     * `function($array, $defaultValue)`.
-     * The possibility to pass an array of keys is available since version 2.0.4.
-     * @param mixed $default the default value to be returned if the specified array key does not exist. Not used when
-     * getting value from an object.
-     * @return mixed the value of the element if found, default value otherwise
+     * NB! This is a copy-paste from Yii2 ArrayHelper
      */
     public static function getValue($array, $key, $default = null)
     {
